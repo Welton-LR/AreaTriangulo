e93b9ad1f4cf440fb64db97455e15db70c4a6d50
Welton <weltonrdesigner@gmail.com>
2024-01-04 17:25:13 -0300

    initial commit

---
+++ MiniProgramas/AreaTriangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+    namespace AreaTriangulo
+    {
+        internal class Program
+        { 
+            static void Main(string[] args)
+            {
+                double area = 0;
+                double alturaTriangulo = 0;
+                double baseTriangulo = 0;
+                string entrada = "";
+                Console.WriteLine("Digite a base");
+                entrada = Console.ReadLine();
+                baseTriangulo = Convert.ToDouble(entrada);
+
+                Console.WriteLine("Digite a altura!");
+                entrada = Console.ReadLine();
+                alturaTriangulo = Convert.ToDouble(entrada);
+
+                area = (baseTriangulo * alturaTriangulo) / 2;
+                Console.WriteLine($"A área do triângulo é {area}");
+                Console.ReadKey();
+           
+
+       
+
+
+
+            }
+        }
+    }
